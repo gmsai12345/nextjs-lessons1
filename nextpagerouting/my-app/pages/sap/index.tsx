00deaa3c736affef4744ea9536d71dd848ebb9e4
gmsai12345 <saidhakshan4@gmail.com>
2023-02-02 18:53:34 +0530

    navigation programatically

--- nextpagerouting/my-app/pages/sap/index.tsx
+++ nextpagerouting/my-app/pages/sap/index.tsx
@@ -1,11 +1,23 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
+import { render } from "react-dom"
 // when we want to click and it has to navigate there we use LINK
-function sap(){
+function sap(){ 
+    const router = useRouter()
+    const handleClick = () => {
+        router.push('/sap/sap2')
+    }
     return <> 
     <h1>sap</h1>
     <Link href='/sap/sap1'>
         <p>blog</p>
         </Link>
+    
+    <button onClick={handleClick}>
+        Place prder
+    </button>
     </>
 }
 export default sap
+// next is navigation programatically 
+// if validation is successful then it has to be rendered automatically
---
+++ nextpagerouting/my-app/pages/sap/sap2.tsx
@@ -0,0 +1,4 @@
+function sap2(){
+    return <h1>sap2</h1>
+}
+export default sap2
