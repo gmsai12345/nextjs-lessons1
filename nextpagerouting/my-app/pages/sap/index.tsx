c8d5e069549bc9fc884dee22a8e82acb69f04afd
gmsai12345 <saidhakshan4@gmail.com>
2023-02-02 18:40:08 +0530

    load that page when we click a word

--- nextpagerouting/my-app/pages/sap/index.tsx
+++ nextpagerouting/my-app/pages/sap/index.tsx
@@ -1,4 +1,11 @@
+import Link from "next/link"
+// when we want to click and it has to navigate there we use LINK
 function sap(){
-    return <h1>sap</h1>
+    return <> 
+    <h1>sap</h1>
+    <Link href='/sap1'>
+        <p>blog</p>
+        </Link>
+    </>
 }
 export default sap
